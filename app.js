c9d596d9bc58999d5025e487179e495d5eb88fbf
lcorozco <luis.orozco@nativo.la>
2022-09-15 12:38:00 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const router = express.Router();
+const port = 3000
+
+/*app.get('/', (req, res) => {
+  res.send('Hello World!')
+})*/
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));    
+});
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
