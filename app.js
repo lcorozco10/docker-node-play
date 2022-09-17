8eb9c844d281fa451f36e0b29cc465af30869721
lcorozco <luis.orozco@nativo.la>
2022-09-16 21:40:52 -0600

    redit added

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express')
-const app = express()
+const redis = require('redis');
 const path = require('path');
+const app = express()
 const router = express.Router();
 const port = 3000
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));    
+let redisClient;
+
+(async () => {
+  redisClient = redis.createClient({
+    socket: {
+      host: 'redis-service',
+      port: 6379
+    }
+  });
+
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+  await redisClient.connect();
+})();
+
+router.get('/', async function (req, res) {
+  const counter = await redisClient.get('counter');
+  console.log(counter);
+  await redisClient.set('counter', counter ? parseInt(counter) + 1 : 1);
+
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.use('/', router);
 
